ddd01e513d64921207edcb1305b00ea04deac7b1
sdasda7777 <17746796+sdasda7777@users.noreply.github.com>
2023-11-24 23:36:21 +0100

    Fixed issues with WASM main variant

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,8 @@ impl Shortcuts {
     }
 }
 
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         ..Default::default()
@@ -83,6 +85,26 @@ fn main() -> Result<(), eframe::Error> {
             Box::<MinesweeperViewController>::default()
         }),
     )
+}
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::<MinesweeperViewController>::default()),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
 
 struct MinesweeperViewController {
